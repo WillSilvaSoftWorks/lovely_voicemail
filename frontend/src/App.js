6835f78751dd2f8b03715165933d64e324bad35b
William Silva <willsilvasoftworks@gmail.com>
2024-11-12 20:52:11 -0500

    Clean up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 // src/App.js
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { fetchData } from './services/api';
 import Backdrop from './components/Backdrop';
 import Navbar from './components/Navbar';
 import ConstructionBanner from './components/ConstructionBanner';
@@ -15,25 +14,13 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 function App() {
-  const [data, setData] = useState('');
   const [isDrawerOpen, setDrawerOpen] = useState(false);
 
   const toggleDrawer = () => {
     setDrawerOpen(!isDrawerOpen);
   };
 
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const result = await fetchData();
-        setData(result.message);
-      } catch (error) {
-        console.error('Error fetching data in App:', error);
-      }
-    };
 
-    getData();
-  }, []);
 
   return (
     <Router>
