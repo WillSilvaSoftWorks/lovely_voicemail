47632a95582a8137be9264211bf26a9778c402ed
William Silva <willsilvasoftworks@gmail.com>
2024-11-13 00:38:21 -0500

    Iframe change

--- frontend/src/pages/Help.js
+++ frontend/src/pages/Help.js
@@ -10,13 +10,23 @@ const Help = () => {
 
         {/* Canva Embed Code for Manual */}
         <div className="manual-embed" style={{ position: 'relative', width: '100%', height: 0, paddingTop: '75%', boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)', marginTop: '1.6em', marginBottom: '0.9em', overflow: 'hidden', borderRadius: '8px', willChange: 'transform' }}>
-          <iframe
-            loading="lazy"
-            style={{ position: 'absolute', width: '100%', height: '100%', top: 0, left: 0, border: 'none', padding: 0, margin: 0 }}
-            src="https://www.canva.com/design/DAGGQa1boHM/PdmJ2D95gLCNwZU5cac-Rg/view?embed"
-            allowFullScreen
-            allow="fullscreen"
-          ></iframe>
+        <iframe
+          loading="lazy"
+          title="Lovely Voicemail Manual" // Add a unique title here
+          style={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            top: 0,
+            left: 0,
+            border: "none",
+            padding: 0,
+            margin: 0,
+          }}
+          src="https://www.canva.com/design/DAGGQa1boHM/PdmJ2D95gLCNwZU5cac-Rg/view?embed"
+          allowFullScreen="allowfullscreen"
+          allow="fullscreen"
+        ></iframe>
         </div>
 
         {/* Link to YouTube video */}
